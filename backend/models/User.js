46214ed3239adfd9ae9f327e3be6c923b2651b28
Jatin17248 <jatinsood242@gmail.com>
2024-06-30 18:31:07 +0530

    Using Try Catch

--- backend/models/User.js
+++ backend/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new Schema({
     
   });
   const User = mongoose.model("user", userSchema);
-  User.createIndexes();
+  
   module.exports = User;
 
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,26 +1,37 @@
-const express = require('express')
-const User = require("../models/User")
-const router = express.Router()
-const {body, validationResult } = require('express-validator');
-router.post('/', [
-  body('name', "Enter A Valid Name!").isLength({min:3}),
-  body('email', "Enter A Valid Email!").isEmail(),
-  body('password', "Enter A Valid Password!").isLength({min:6}),
-],(req, res) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-          return res.status(400).json({ errors: errors.array() })
-        }
-       User.create({
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter A Valid Name!").isLength({ min: 3 }),
+    body("email", "Enter A Valid Email!").isEmail(),
+    body("password", "Enter A Valid Password!").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(500)
+          .json({ error: "Sorry, User With This E-Mail Already Exists!" });
+      }
+      user = await User.create({
         name: req.body.name,
         password: req.body.password,
-        email: req.body.email
-       }).then(user=>res.json(user)).catch(err => {res.status(400)
-         res.json({errors: "User With Same E-mail Already Exists!"})});
-     })
-    // app.get('/name', (req, res) => {
-    //     res.send('Jatin')
-    //   })
-  
+        email: req.body.email,
+      });
+      res.json({ user });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({error: "Some Error Happened"});
+    }
+  }
+);
 
-    module.exports = router
+module.exports = router;
